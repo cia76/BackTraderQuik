cf70a1ae7d154416b79bc84ef084e8009633f9a4
Игорь Чечет <ichechet@mail.ru>
2022-01-14 14:44:15 +0500

    Переводим в LIVE когда на открытии сессии приходит последний бар прошлой сессии

--- QKData.py
+++ QKData.py
@@ -52,14 +52,14 @@ class QKData(with_metaclass(MetaQKData, AbstractDataBase)):
         super(QKData, self).start()
         if self.p.tz is None:  # Если временнАя зона не указана
             self.p.tz = QKStore.MarketTimeZone  # то берем московское время биржи
-            # HACK Хоть мы и задаем временнУю зону биржи, но параметры fromdate и todate переводятся в GMT
-            # Поэтому, считаем, что время задается в GMT, переводим его во время биржи и удаляем временнУю зону
-            if self.p.fromdate is not None:  # Если задана дата начала получения исторических данных
-                dt = self.p.fromdate = pytz.utc.localize(self.p.fromdate).astimezone(self.p.tz)
-                self.p.fromdate = dt.replace(tzinfo=None)
-            if self.p.todate is not None:  # Если задана дата окончания получения исторических данных
-                dt = self.p.todate = pytz.utc.localize(self.p.todate).astimezone(self.p.tz)
-                self.p.todate = dt.replace(tzinfo=None)
+        # HACK Хоть мы и задаем временнУю зону биржи, но параметры fromdate и todate переводятся в GMT
+        # Поэтому, считаем, что время задается в GMT, переводим его во время биржи и удаляем временнУю зону
+        if self.p.fromdate is not None:  # Если задана дата начала получения исторических данных
+            dt = self.p.fromdate = pytz.utc.localize(self.p.fromdate).astimezone(self.p.tz)
+            self.p.fromdate = dt.replace(tzinfo=None)
+        if self.p.todate is not None:  # Если задана дата окончания получения исторических данных
+            dt = self.p.todate = pytz.utc.localize(self.p.todate).astimezone(self.p.tz)
+            self.p.todate = dt.replace(tzinfo=None)
         if self.p.sessionstart is None:  # Если время начала сессии не указано
             self.p.sessionstart = datetime.time(10, 00)  # то берем время начала сессии на бирже
         if self.p.sessionend is None:  # Если время окончания сессии не указано
@@ -76,15 +76,15 @@ class QKData(with_metaclass(MetaQKData, AbstractDataBase)):
         dt = datetime(jsonDateTime['year'], jsonDateTime['month'], jsonDateTime['day'], jsonDateTime['hour'], jsonDateTime['min'])  # Время открытия бара
         timeOpen = self.p.tz.localize(dt)  # Биржевое время открытия бара
         timeClose = timeOpen + timedelta(minutes=self.interval)  # Биржевое время закрытия бара
-        timeMarketNow = datetime.now(self.p.tz)  # Текущее биржевое время
+        timeMarketNow = datetime.now(self.p.tz)  # Берем текущее время на рынке из локального
         if timeClose > timeMarketNow and timeMarketNow.time() < self.p.sessionend:  # Если время закрытия бара еще не наступило на бирже, и сессия еще не закончилась
             self.lastBarId -= 1  # то последний бар из истории не принимаем
 
     def _load(self):
         """Загружаем бар из истории или новый бар в BackTrader"""
         if self.newCandleSubscribed:  # Если получаем новые бары по подписке
-            if len(self.store.newBars) == 0:  # Если в хранилище новых баров ничего нет
-                return None  # то нового бара нет, будем заходить еще
+            if len(self.store.newBars) == 0:  # Если в хранилище никаких новых баров нет
+                return None  # то и нового бара нет, будем заходить еще
             newBars = [newBar for newBar in self.store.newBars  # Смотрим в хранилище новых баров
                        if newBar['class'] == self.classCode and  # бары с нужным кодом площадки,
                        newBar['sec'] == self.secCode and  # тикером,
@@ -101,6 +101,18 @@ class QKData(with_metaclass(MetaQKData, AbstractDataBase)):
             dtMarketNow = self.GetQUIKDateTimeNow()  # Текущее биржевое время из QUIK
             if dtMarketBarClose > dtMarketNow:  # Если получили несформированный бар. Например, дневной бар в середине сессии
                 return None  # то нового бара нет, будем заходить еще
+            dtMarketNextBarClose = dt + timedelta(minutes=self.interval * 2)  # Биржевое время закрытия следующего бара
+            dtMarketNow = self.GetQUIKDateTimeNow()  # Текущее биржевое время из QUIK
+            # Переходим в режим получения новых баров (LIVE), если не находимся в этом режиме и
+            # следующий бар закроется в будущем (т.к. пришедший бар закрылся в прошлом), или пришел последний бар предыдущей сессии
+            if not self.liveMode and (dtMarketNextBarClose > dtMarketNow or dt.day != dtMarketNow.day):
+                self.put_notification(self.LIVE)  # Отправляем уведомление о получении новых баров
+                self.liveMode = True  # Переходим в режим получения новых баров (LIVE)
+            # Бывает ситуация, когда QUIK несколько минут не передает новые бары. Затем передает все пропущенные
+            # Чтобы не совершать сделки на истории, меняем режим торгов на историю до прихода нового бара
+            elif self.liveMode and dtMarketNextBarClose <= dtMarketNow:  # Если в режиме получения новых баров, и следующий бар закроется до текущего времени на бирже
+                self.put_notification(self.DELAYED)  # Отправляем уведомление об отправке исторических (не новых) баров
+                self.liveMode = False  # Переходим в режим получения истории
         else:  # Если получаем исторические данные
             if len(self.jsonBars) == 0:  # Если исторических данных нет (QUIK отключен от сервера брокера)
                 self.put_notification(self.DISCONNECTED)  # Отправляем уведомление об окончании получения исторических баров
@@ -110,7 +122,6 @@ class QKData(with_metaclass(MetaQKData, AbstractDataBase)):
                 if not self.p.LiveBars:  # Если новые бары не принимаем
                     return False  # Больше сюда заходить не будем
                 # Принимаем новые бары
-                self.jsonBar = None  # Сбрасываем последний бар истории, чтобы он не дублировался как новый бар
                 if not self.store.qpProvider.IsSubscribed(self.classCode, self.secCode, self.interval)['data']:  # Если не было подписки на тикер/интервал
                     self.store.qpProvider.SubscribeToCandles(self.classCode, self.secCode, self.interval)  # Подписываемся на новые бары
                     self.store.subscribedSymbols.append({'class': self.classCode, 'sec': self.secCode, 'interval': self.interval})  # Добавляем в список подписанных тикеров/интервалов
@@ -118,34 +129,19 @@ class QKData(with_metaclass(MetaQKData, AbstractDataBase)):
                 self.newCandleSubscribed = True  # Дальше будем получать новые бары по подписке
                 return None  # Будем заходить еще
             else:  # Если еще не получили все бары из истории
-                self.jsonBar = self.jsonBars[self.barId]  # Получаем следующий бар из истории
-        # Исторический / новый бар
+                self.jsonBar = self.jsonBars[self.barId]  # Получаем текущий бар из истории
+                self.barId += 1  # Перемещаем указатель на следующий бар
+        # Записываем полученный исторический / новый бар
         jsonDateTime = self.jsonBar['datetime']  # Вытаскиваем составное значение даты и времени открытия бара
         dt = datetime(jsonDateTime['year'], jsonDateTime['month'], jsonDateTime['day'], jsonDateTime['hour'], jsonDateTime['min'])  # Время открытия бара
         self.lines.datetime[0] = date2num(dt)  # Переводим в формат хранения даты/времени в BackTrader
-        self.lines.open[0] = self.store.QKToBTPrice(self.classCode, self.secCode, self.jsonBar['open'])
-        self.lines.high[0] = self.store.QKToBTPrice(self.classCode, self.secCode, self.jsonBar['high'])
-        self.lines.low[0] = self.store.QKToBTPrice(self.classCode, self.secCode, self.jsonBar['low'])
-        self.lines.close[0] = self.store.QKToBTPrice(self.classCode, self.secCode,  self.jsonBar['close'])
-        self.lines.volume[0] = self.jsonBar['volume']
+        self.lines.open[0] = self.store.QKToBTPrice(self.classCode, self.secCode, self.jsonBar['open'])  # Open
+        self.lines.high[0] = self.store.QKToBTPrice(self.classCode, self.secCode, self.jsonBar['high'])  # High
+        self.lines.low[0] = self.store.QKToBTPrice(self.classCode, self.secCode, self.jsonBar['low'])  # Low
+        self.lines.close[0] = self.store.QKToBTPrice(self.classCode, self.secCode,  self.jsonBar['close'])  # Close
+        self.lines.volume[0] = self.jsonBar['volume']  # Volume
         self.lines.openinterest[0] = 0  # Открытый интерес в QUIK не учитывается
-        # Исторический бар
-        if self.barId <= self.lastBarId:  # Если еще не получили все бары из истории
-            self.barId += 1  # то переходим на следующий бар
-            return True  # Будем заходить сюда еще
-        # Новый бар
-        dtMarketNextBarClose = dt + timedelta(minutes=self.interval*2)  # Биржевое время закрытия следующего бара
-        dtMarketNow = self.GetQUIKDateTimeNow()  # Текущее биржевое время из QUIK
-        if not self.liveMode and dtMarketNextBarClose > dtMarketNow:  # Если не в режиме получения новых баров, и следующий бар закроется позже текущего времени на бирже
-            self.put_notification(self.LIVE)  # Уведомляем о получении новых баров
-            self.liveMode = True  # Переходим в режим получения новых баров
-        # Бывает ситуация, когда QUIK несколько минут не передает новые бары. Затем передает все пропущенные
-        # Чтобы не совершать сделки на истории, меняем режим торгов на историю до прихода нового бара
-        elif self.liveMode and dtMarketNextBarClose <= dtMarketNow:  # Если в режиме получения новых баров, и следующий бар закроется до текущего времени на бирже
-            self.put_notification(self.DELAYED)  # Отправляем уведомление об отправке исторических (не новых) баров
-            self.liveMode = False  # Переходим в режим получения истории
-        self.jsonBar = None  # Сбрасываем текущий бар
-        return True  # Будем заходить еще
+        return True  # Будем заходить сюда еще
 
     def stop(self):
         super(QKData, self).stop()
